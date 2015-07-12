08a7c9af2391d73760f5e258b16fa9000c4cb095
Chris Klosowski <cklosowski@gmail.com>
2015-07-12 10:01:28 -0700

    Allow usermeta for no co-op fees

--- bawlersncrawlers-mods.php
+++ bawlersncrawlers-mods.php
@@ -38,6 +38,17 @@ class BNC_Mods {
 		add_action( 'woocommerce_cart_calculate_fees',array( $this, 'add_paypal_fees_to_cart' ), 999 );
 		add_filter( 'woocommerce_product_is_visible', array( $this, 'show_backorders' ), 10, 2 );
 		remove_action( 'woocommerce_product_on_backorder_notification', array( 'WC_Emails', 'backorder' ) );
+
+		// Show the user profile fields
+		add_action( 'show_user_profile', array( $this, 'profile_fields' ), 99, 1 );
+		add_action( 'edit_user_profile', array( $this, 'profile_fields' ), 99, 1 );
+
+		// Save the user profile fields
+		add_action( 'personal_options_update', array( $this, 'save_profile_fields' ) );
+		add_action( 'edit_user_profile_update', array( $this, 'save_profile_fields' ) );
+
+		// Check if the person is charged co-op fees when fees are being calculated
+		add_filter( 'wc_cat_fees_amount', array( $this, 'maybe_remove_fees' ), 10, 2 );
 	}
 
 	public function add_paypal_fees_to_cart() {
@@ -67,6 +78,69 @@ class BNC_Mods {
 		}
 
 		return $is_visible;
+
+	}
+
+	/**
+	 * Adds in the Site Specific feeds notice
+	 * @param  object $user The User object being viewed
+	 * @return void         Displays HTML
+	 */
+	public function profile_fields( $user ) {
+		if ( $user->ID !== get_current_user_id() || ! current_user_can( 'manage_options' ) ) {
+			return;
+		}
+		?>
+		<h3><?php _e( 'Bawlers N Crawlers Settings', 'bnc-mods' ); ?></h3>
+		<table class="form-table">
+			<tr>
+				<th><?php _e( 'No co-op fees', 'bnc-mods' ); ?></th>
+				<td>
+				<?php $no_fees = get_user_meta( $user->ID, '_bnc_no_coop_fees', true ); ?>
+				<input type="checkbox" id="no-fees" name="bnc_no_coop_fees" value="1" <?php checked( true, $no_fees, true ); ?> /><label for="no-fees"><?php _e( 'User does not pay co-op fees.', 'bnc-mods' ); ?></label>
+				<p class="description"><?php _e( 'When checked, the user will not pay co-op fees, but does pay service fees.', 'bnc-mods' ); ?></p>
+				</td>
+			</tr>
+		</table>
+		<?php
+	}
+
+	/**
+	 * Saves the User Profile Settings
+	 * @param  int $user_id The User ID being saved
+	 * @return void         Saves to Usermeta
+	 */
+	public function save_profile_fields( $user_id ) {
+		$no_fees = false;
+
+		if ( ! empty( $_POST['bnc_no_coop_fees'] ) ) {
+			$no_fees = true;
+		}
+
+		update_user_meta( $user_id, '_bnc_no_coop_fees', $no_fees );
+	}
+
+	/**
+	 * Maybe avoid adding fees for specific users who have been labeled to not get fees
+	 *
+	 * @param  float $fee_amount The amount of the fee
+	 * @param  int $fee_item     The ID of the item that is associated with the fee
+	 * @return float             The fee amount
+	 */
+	public function maybe_remove_fees( $fee_amount, $fee_item ) {
+
+		if ( empty( $fee_amount ) ) {
+			return $fee_amount;
+		}
+
+		$user_id = get_current_user_id();
+		$no_coop_fees = get_user_meta( $user_id, '_bnc_no_coop_fees', true );
+
+		if ( ! empty( $no_coop_fees ) ) {
+			return 0;
+		}
+
+		return $fee_amount;
 
 	}
 
